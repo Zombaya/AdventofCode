556c00d3797932fc08ba0079f606b83d413fc6c9
Zombaya <Zombaya01@gmail.com>
2015-12-10 01:10:38 +0100

    Day 1 part 1

--- AdventofCode/Program.cs
+++ AdventofCode/Program.cs
@@ -10,6 +10,20 @@ namespace AdventofCode
     {
         static void Main(string[] args)
         {
+            do
+            {
+                int i = 0;
+                char c = ' ';
+                while ((c = Console.ReadKey().KeyChar) != ' ')
+                {
+                    if (c == ')')
+                        i--;
+                    if (c == '(')
+                        i++;
+                }
+                Console.WriteLine("\n\nResult is: " + i + "\n\n");
+            }
+            while (Console.ReadKey().Key != ConsoleKey.S);
         }
     }
 }

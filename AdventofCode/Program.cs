5c579f2f64bbfb11f580fded710c64bd60a1a268
Zombaya <Zombaya01@gmail.com>
2015-12-13 22:38:41 +0100

    Day 8 - 2

--- AdventofCode/Program.cs
+++ AdventofCode/Program.cs
@@ -14,12 +14,8 @@ namespace AdventofCode
     {
         static void Main(string[] args)
         {
-            int total = 0;
-            int nice = 0;
-            int naughty = 0;
-            int[,] lichtjes = new int[1000, 1000];
-            int xmin, xmax, ymin, ymax;
-            string[] nummers;
+            int totaloriginal = 0;
+            int totalencoded = 0;
 
             string line;
             Console.WriteLine("Enter one word (press CTRL+Z to exit):");
@@ -27,63 +23,18 @@ namespace AdventofCode
             line = Console.ReadLine();
             Stopwatch stopwatch = new Stopwatch();
             stopwatch.Start();
-            while (line != null)
+            while (line != null && !line.Equals(""))
             {
-                string[] values = line.Split(' ');
-                if (values[0].Equals("turn"))
-                {
-                    nummers = values[2].Split(',');
-                    xmin = Int32.Parse(nummers[0]);
-                    ymin = Int32.Parse(nummers[1]);
-                    nummers = values[4].Split(',');
-                    xmax = Int32.Parse(nummers[0]);
-                    ymax = Int32.Parse(nummers[1]);
+                string encoded = encode(line);
 
-                    if (values[1].Equals("on"))
-                    {
-                        for (int i = xmin; i <= xmax; i++)
-                        {
-                            for (int j = ymin; j <= ymax; j++)
-                            {
-                                lichtjes[i, j] += 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        for (int i = xmin; i <= xmax; i++)
-                        {
-                            for (int j = ymin; j <= ymax; j++)
-                            {
-                                if(lichtjes[i, j] != 0)
-                                    lichtjes[i, j] -= 1;
-                            }
-                        }
-                    }
-                }
-                else if (values[0].Equals("toggle"))
-                {
-                    nummers = values[1].Split(',');
-                    xmin = Int32.Parse(nummers[0]);
-                    ymin = Int32.Parse(nummers[1]);
-                    nummers = values[3].Split(',');
-                    xmax = Int32.Parse(nummers[0]);
-                    ymax = Int32.Parse(nummers[1]);
-                    for (int i = xmin; i <= xmax; i++)
-                    {
-                        for (int j = ymin; j <= ymax; j++)
-                        {
-                            lichtjes[i, j] += 2;
-                        }
-                    }
-                }
-                else
-                    break;
+                totaloriginal += line.Length;
+                totalencoded += encoded.Length;
 
-                Console.WriteLine("\tTotaal lichtjes: "+ getTotal(lichtjes));
+                Console.WriteLine("\t Totaal encoded: {0}\toriginal: {1}", line.Length, encoded.Length);
+
                 line = Console.ReadLine();
             }
-            Console.WriteLine("Totaal lichtjes: "+getTotal(lichtjes));
+            Console.WriteLine("Totaal encoded: {0} - original: {1} = {2}", totaloriginal, totalencoded, totaloriginal - totalencoded);
             Console.WriteLine("loop time in milliseconds: {0}",
                                 stopwatch.ElapsedMilliseconds);
 
@@ -93,103 +44,20 @@ namespace AdventofCode
             Console.ReadLine();
 
         }
-        static string getTotal(int[,] lichtjes)
+
+        static string encode(string s)
         {
-            int total = 0;
-            for (int i = 0; i < 1000; i++)
-            {
-                for (int j = 0; j < 1000; j++)
-                {
-                    total += lichtjes[i, j] ;
-                }
-            }
-            return total.ToString() ;
-        }
-
-        static string GetMd5Hash(MD5 md5Hash, string input)
-        {
-
-            // Convert the input string to a byte array and compute the hash.
-            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
-
-            // Create a new Stringbuilder to collect the bytes
-            // and create a string.
-            StringBuilder sBuilder = new StringBuilder();
-
-            // Loop through each byte of the hashed data 
-            // and format each one as a hexadecimal string.
-            for (int i = 0; i < data.Length; i++)
-            {
-                sBuilder.Append(data[i].ToString("x2"));
-            }
-
-            // Return the hexadecimal string.
-            return sBuilder.ToString();
-        }
-
-        public static bool IsNice1(string s)
-        {
-            bool isnice = false;
-            for(int i=0; i< (s.Length - 1); i++)
-            { 
-                string sub = s.Substring(i, 2);
-                if (sub.Equals("ab") || sub.Equals("cd") || sub.Equals("pq") || sub.Equals("xy"))
-                {
-                    return false;
-                }
-            }
-
-            for (int i = 0; i < (s.Length - 1); i++)
-            {
-                string sub = s.Substring(i, 2);
-                if (sub[0] == sub[1])
-                    isnice=true;
-            }
-            if (!isnice)
-                return false;
-
-            int vowels = 0;
-            for (int i = 0; i < s.Length; i++)
+            string result = "";
+            for(int i = 0; i < s.Length; i++)
             {
                 switch (s[i])
                 {
-                    case 'a': case 'e': case 'i': case 'o': case 'u': vowels++; break;
-                    default: break;   
+                    case '"': result += "\\\""; break;
+                    case '\\': result += "\\\\"; break;
+                    default: result += s[i]; break;
                 }
             }
-            if (vowels >= 3) return true;
-
-
-            return false;
+            return '"' + result + '"';
         }
-
-        public static bool IsNice2(string s)
-        {
-            bool isniceTo1 = false;
-            for (int i = 0; i < (s.Length - 1); i++)
-            {
-                string sub = s.Substring(i, 2);
-                for (int j = i + 2; j < (s.Length - 1) ; j++)
-                {
-                    if (sub.Equals(s.Substring(j, 2))){
-                        isniceTo1 = true;
-                        break;
-                    }
-                }
-            }
-            if (!isniceTo1)
-                return false;
-
-            for(int i=0; i< (s.Length - 2); i++)
-            {
-                if (s[i] == s[i + 2])
-                    return true;
-            }
-
-            return false;
-        }
-
     }
-
-   
 }
